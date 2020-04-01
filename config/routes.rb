eb0048651b0d8fcc6cf1c9eab765a21e54cd4a6f
jones-sam <samjones6058@gmail.com>
2020-04-01 12:56:34 -0300

    completed app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/tasks')
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
